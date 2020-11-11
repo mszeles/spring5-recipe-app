64f74757bf41fd0b8c21f07fda47cb4992d6465c
mszeles <miklos.szeles@gmail.com>
2020-11-11 20:51:22 +0100

    Display a recipe by ID.

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,11 +1,10 @@
 package guru.springframework.services;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -28,6 +27,22 @@ class RecipeServiceImplTest {
 		MockitoAnnotations.initMocks(this);
 		recipeService = new RecipeServiceImpl(recipeRepository);
 	}
+	
+	@Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull(recipeReturned, "Null recipe returned");
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+	
 	@Test
 	void testGetRecipes() {
 		Recipe recipe = new Recipe();
